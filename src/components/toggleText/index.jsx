841377766c0e904fda0c10e04b0d8f456463d8ae
DEN21R <denr@ukr.net>
2025-11-22 11:25:59 +0200

    added styles

--- src/components/toggleText/index.jsx
+++ src/components/toggleText/index.jsx
@@ -15,28 +15,27 @@ function ToggleText() {
   const visibilityChange = () => {
     if (!isVisib) {
       setIsVisib(true)
-      textRef.current.style.maxHeight = '200px'
+      textRef.current.style.maxHeight = '300px'
       textRef.current.style.opacity = '1'
       textRef.current.style.transition = `all ${duration}s`
     } else {
       setIsVisib(false)
-      textRef.current.style.maxHeight = '0px'
+      textRef.current.style.maxHeight = '300px'
       textRef.current.style.opacity = '0'
       textRef.current.style.transition = `all ${duration}s`
     }
   }
 
   return (
-    <div>
-      <div>
-        <input type="text" onChange={inputValue} />
-        <p ref={textRef} className={styles.textBlock}>
-          The useRef Hook allows you to persist values between renders. It can
-          be used to store a mutable value that does not cause a re-render when
-          updated. It can be used to access a DOM element directly.
-        </p>
-        <button onClick={visibilityChange}>Toggle</button>
-      </div>
+    <div className={styles.container}>
+      <h1>Select how many seconds the message will appear</h1>
+      <input type="text" onChange={inputValue} />
+      <p ref={textRef} className={styles.textBlock}>
+        The useRef Hook allows you to persist values between renders. It can be
+        used to store a mutable value that does not cause a re-render when
+        updated. It can be used to access a DOM element directly.
+      </p>
+      <button onClick={visibilityChange}>Toggle</button>
     </div>
   )
 }
